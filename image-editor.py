0f210ba878c9ddf0e619953b0f0fd16be27c2b12
tum0xa <timofey.samodurov@mail.ru>
2019-10-02 17:45:53 +0500

    Added toolbar

--- image-editor.py
+++ image-editor.py
@@ -1,9 +1,13 @@
 import sys
 import os
-
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QAction, QMainWindow, QFileDialog, QHBoxLayout, QToolButton, QToolBar
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QAction, QMainWindow, QFileDialog, QHBoxLayout, QToolButton, \
+    QToolBar
 
 from PyQt5.QtGui import QIcon, QPixmap
+
+from PIL import Image, ImageDraw
+from PIL.ImageQt import ImageQt
 
 
 # from PyQt5.QtGui import QPixmap, QImage, qRgb
@@ -11,52 +15,74 @@ from PyQt5.QtGui import QIcon, QPixmap
 
 class ImageEditor(QMainWindow):
 
-    def __init__(self):
+    def __init__(self, app):
         super().__init__()
-
+        self.app = app
         self.initUI()
 
     def initUI(self):
-        self.label = QLabel(self)
 
-        openFile = QAction(QIcon('open.png'), 'Open', self)
+
+        openFile = QAction(QIcon('assets/open.png'), 'Open', self)
         openFile.setShortcut('Ctrl+O')
-        openFile.setStatusTip('Открыть файл')
-        openFile.triggered.connect(self.showImageLoadDialog)
+        openFile.setStatusTip('&Open file')
+        openFile.triggered.connect(self.show_image_load_dialog)
 
         hbox = QHBoxLayout(self)
 
-        hbox.addWidget(self.label)
+
         self.setLayout(hbox)
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&Файл')
-        fileMenu.addAction(openFile)
 
+        self.menubar = self.menuBar()
+        file_menu = self.menubar.addMenu('&File')
+        file_menu.addAction(openFile)
 
+        self.label = QLabel()
 
-        toolbar = QToolBar()
-        tbSepia = QToolButton()
-        toolbar.addWidget(tbSepia)
-        self.addToolBar(toolbar)
-
+        self.toolbar = QToolBar()
+        self.toolbar.addAction(openFile)
+        self.addToolBar(self.toolbar)
+        # hbox.addWidget(self.menubar)
+        hbox.addWidget(self.label)
         self.resize(640, 480)
-        self.move(300, 200)
+        self.set_pos_to_center_of_screen()
         self.setWindowTitle('Simple Image Editor')
         self.show()
 
-    def showImageLoadDialog(self):
-        fileDialog = QFileDialog()
-        fileName = fileDialog.getOpenFileName(self, 'Load image', '', 'Image files (*.jpg *.png *.bmp)')[0]
+    def show_image_load_dialog(self):
+        file_dialog = QFileDialog()
+        file_name = file_dialog.getOpenFileName(self, 'Load image', '', 'Image files (*.jpg *.png *.bmp)')[0]
+        if file_name:
+            self.setWindowTitle(f'Image editor - {file_name}')
+            self.image = Image.open(file_name)
+            # draw = ImageDraw.Draw(self.image)
+            img_tmp = ImageQt(self.image.convert('RGBA'))
+            pixmap = QPixmap.fromImage(img_tmp)
+            w, h = pixmap.width(), pixmap.height()
+            # self.label.move(w, self.toolbar.height())
+            self.label.move()
 
-        pixmap = QPixmap(fileName)
-        w, h = pixmap.width(), pixmap.height()
-        self.label.resize(w, h)
+            self.label.resize(w, h)
+            # self.adjustSize()
+            self.resize(w, h)
+            self.set_pos_to_center_of_screen()
+            self.label.setPixmap(pixmap)
 
-        self.resize(w, h)
-        self.label.setPixmap(pixmap)
+    def get_screen_size(self):
+        screen = self.app.primaryScreen()
+        screen_size = screen.size()
+        screen_width = screen_size.width()
+        screen_height = screen_size.height()
+        return screen_width, screen_height
+
+    def set_pos_to_center_of_screen(self):
+        screen_width, screen_height = self.get_screen_size()
+        window_width = self.width()
+        window_height = self.height()
+        self.move((screen_width - window_width) / 2, (screen_height - window_height) / 2)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    image_editor = ImageEditor()
+    image_editor = ImageEditor(app)
     sys.exit(app.exec_())
